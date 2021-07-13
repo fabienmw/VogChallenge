d0e33ec5d9286e5ed2d934c58260a0b7af0bcfca
Fabien <fabien.mwamba@itq.co.za>
2021-07-13 23:56:23 +0200

    Added a SQL db provider implementation

---
+++ VogCodeChallenge.API/DataLayer/SQLDbProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Entities;
+
+namespace VogCodeChallenge.API.DataLayer
+{
+    public class SQLDbProvider : IDbProvider
+    {
+        //NOTE: here you can make use of EF core or Dapper as your ORM and implement the below methods.
+        public IEnumerable<Employee> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Employee> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
