cfb5ac9d9f2f440e8ae93269af5c318a6d4be42a
Fabien <fabien.mwamba@itq.co.za>
2021-07-13 21:49:38 +0200

    Added entities

---
+++ VogCodeChallenge.API/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        IEnumerable<Employee> Employees { get; set; }
+    }
+}
