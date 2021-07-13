7e1e634b95d723830513b5ab6bac6a57cf0bae68
Fabien <fabien.mwamba@itq.co.za>
2021-07-13 22:56:13 +0200

    Refactored in memory data provider

--- VogCodeChallenge.API/Services/EmployeeService.cs
+++ VogCodeChallenge.API/Services/EmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using VogCodeChallenge.API.DataLayer;
 using VogCodeChallenge.API.Entities;
 
@@ -12,21 +13,26 @@ namespace VogCodeChallenge.API.Services
         {
             _dbProvider = dbProvider;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
+
         public IEnumerable<Employee> GetAll()
         {
+            //Suggestion: Introduce a DTO or ViewModel and map only the fields that should be exposed rather than returning the result as is which expose the table or document structure
+
             return _dbProvider.GetEmployees();
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
+
         public IList<Employee> ListAll()
         {
             return _dbProvider.ListAll();
+        }
+
+        public IEnumerable<Employee> RetrieveEmployeesByDepartment(int departmentId)
+        {
+            var employees = _dbProvider.GetEmployees();
+
+            var employeesByDepartment = employees.Where(e => e.DepartmentId == departmentId);
+
+            return employeesByDepartment;
         }
     }
 }
