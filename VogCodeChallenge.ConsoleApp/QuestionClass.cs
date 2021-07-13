84de2742c4d01935a3b3648a8eab005ce7e57ab7
Fabien <fabien.mwamba@itq.co.za>
2021-07-14 00:25:59 +0200

    Iterate through a list without for loops

---
+++ VogCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class QuestionClass
+    {
+        static List<string> NameList = new List<string>
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John"
+        };
+
+        public static void IterateWithoutForLoops()
+        {
+            int index = 0;
+            while (index < NameList.Count)
+            {
+                Console.WriteLine(NameList[index]);
+                index++;
+            }
+        }
+    }
+}
