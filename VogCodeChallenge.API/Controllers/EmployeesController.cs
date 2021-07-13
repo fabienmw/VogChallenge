0a4863771d9d3feebb11d85cc85a50680ff2bbaf
Fabien <fabien.mwamba@itq.co.za>
2021-07-13 22:39:18 +0200

    Added Employee controller

---
+++ VogCodeChallenge.API/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Services;
+
+namespace VogCodeChallenge.API.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        [HttpGet("Employees")]
+        public async Task<IActionResult> RetrieveAllEmployees()
+        {
+            var result = _employeeService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("Employees/department/{departmentId}")]
+        public async Task<IActionResult> RetrieveEmployeesByDepartment([FromRoute] int departmentId)
+        {
+            return Ok();
+        }
+    }
+}
