e13e20fb978f63a9ea88650f9a4c2582e744658f
Fabien <fabien.mwamba@itq.co.za>
2021-07-13 22:34:35 +0200

    Added initial employee service implementation

--- VogCodeChallenge.API/Services/EmployeeService.cs
+++ VogCodeChallenge.API/Services/EmployeeService.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using VogCodeChallenge.API.DataLayer;
+using VogCodeChallenge.API.Entities;
 
 namespace VogCodeChallenge.API.Services
 {
-    public class Class
+    public class EmployeeService : IEmployeeService
     {
+        private readonly IDbProvider _dbProvider;
+        public EmployeeService(IDbProvider dbProvider)
+        {
+            _dbProvider = dbProvider;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Employee> GetAll()
+        {
+            return _dbProvider.GetEmployees();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IList<Employee> ListAll()
+        {
+            return _dbProvider.ListAll();
+        }
     }
 }
---
+++ VogCodeChallenge.API/Services/IEmployeeService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using VogCodeChallenge.API.Entities;
+
+namespace VogCodeChallenge.API.Services
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAll();
+        IList<Employee> ListAll();
+    }
+}
