2a4fdc0ff193f8105b1c7d40a51a050ab4b58741
Fabien <fabien.mwamba@itq.co.za>
2021-07-13 23:57:38 +0200

    Updated comment

--- VogCodeChallenge.API/DataLayer/SQLDbProvider.cs
+++ VogCodeChallenge.API/DataLayer/SQLDbProvider.cs
@@ -8,7 +8,7 @@ namespace VogCodeChallenge.API.DataLayer
 {
     public class SQLDbProvider : IDbProvider
     {
-        //NOTE: here you can make use of EF core or Dapper as your ORM and implement the below methods.
+        //SUGGESTION: here you can make use of EF core or Dapper as your ORM and implement the below methods.
         public IEnumerable<Employee> GetEmployees()
         {
             throw new NotImplementedException();
